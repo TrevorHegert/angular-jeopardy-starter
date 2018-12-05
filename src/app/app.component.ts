b7c1afb103fd0c78cddf0b8eea89ce6b39d860be
Trevor Hegert <thegert138@gmail.com>
2018-12-04 21:17:00 -0500

    Angular Jeopardy, no Styles Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
-import { DataService } from './data.service'
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent implements OnInit {
-  title = 'app';
+  title = "Trevor's Angular Jeopardy Game"
 
   questionInfo;
+  userAnswer: string = "";
+  userScore: number = 0;
+  lastAnswerWas: string = '';
+  cheating: boolean = false;
+  
 
   constructor(private DataService: DataService){}
 
@@ -19,8 +25,42 @@ export class AppComponent implements OnInit {
       .subscribe(
         questionInfo => {
           this.questionInfo = questionInfo[0];
-        }
+                  }
       )
+  }
+
+  checkAnswer(){
+    if (this.userAnswer.toLowerCase === this.questionInfo.answer.toLowerCase) {
+      this.givePoints();
+    } else {
+      this.takePoints();
+    }
+  }
+
+  takePoints(){
+    this.lastAnswerWas = 'INCORRECT!';
+    this.userScore = (this.userScore - this.questionInfo.value);
+    this.DataService.getQuestionInfo()
+      .subscribe(
+        questionInfo => {
+          this.questionInfo = questionInfo[0];
+                  }
+      )
+  }
+
+  givePoints(){
+    this.lastAnswerWas = 'CORRECT!;'
+    this.userScore = (this.userScore + this.questionInfo.value);
+    this.DataService.getQuestionInfo()
+      .subscribe(
+        questionInfo => {
+          this.questionInfo = questionInfo[0];
+                  }
+      )
+  }
+
+  cheat() {
+    this.cheating = !this.cheating;
   }
 
   ngOnInit(){
